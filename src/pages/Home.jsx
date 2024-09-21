f897145e2e71a3bfb666d02326e346eede4f9ad8
Anurag <anuragtupkar168@gmail.com>
2024-09-21 20:09:40 +0530

    add Delete feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <div className="">
+      <div className="py-16 px-14 ">
+        <h1 className="mb-60 text-6xl uppercase font-bold text-center">Small Projects</h1>
+        
+        
         <Link
           to="todo"
           className="p-4 border-black border rounded-lg bottom-8 hover:text-white hover:bg-black mx-8 w-32"
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -4,16 +4,25 @@ import { nanoid } from 'nanoid';
 function Todo() {
   const [task, setTask] = useState('');
   const [list, setList] = useState([]);
-
+  const [check, setCheck] = useState("bg-blue-500")
   const addTask = (e) => {
-    e.preventDefault(); // Moved e.preventDefault() to the top
+    e.preventDefault(); 
 
     if (task.trim()) {
-      setList([...list, { id: nanoid(), task }]); // Add new task with nanoid
-      setTask(''); // Clear the input after adding
+      setList([...list, { id: nanoid(), task }]); 
+      setTask(''); 
     }
   };
+  const deleteTask = (id)=>{
+    let updatedList = list.filter((e)=> e.id != id)
+    setList(updatedList)
+  }
 
+  const completedTask = (id)=>{
+    let taskStatus = check == "bg-blue-500" ? "" : "bg-blue-500"
+    // taskStatus = taskStatus.filter((e)=>e.id != id)
+    setCheck(taskStatus);
+  }
   return (
     <>
       <div className="flex px-4 py-6 justify-center flex-col">
@@ -23,8 +32,8 @@ function Todo() {
             <input
               className="border-2 w-96 h-14 p-4 todo rounded-md shadow"
               placeholder="Enter text"
-              value={task} // Bind the input value to task state
-              onChange={(e) => setTask(e.target.value)} // Update task state on change
+              value={task} 
+              onChange={(e) => setTask(e.target.value)}
             />
             <button
               className="p-4 border-t-emerald-600 border rounded-lg bottom-8 hover:text-white hover:bg-black mx-8 w-32"
@@ -37,17 +46,17 @@ function Todo() {
 
         <div className="list">
           {list.map((e) => (
+            
             <div key={e.id} className="toDoList flex justify-center p-2">
-              {/* Display the task text */}
-              <h1 className="w-96 h-14 font-bold rounded-md pt-3 pl-4 mr-10 items-center border text-black text-lg">
+              <h1 className={`w-96 h-14 font-bold rounded-md pt-3 pl-4 mr-10 items-center border  ${check} text-lg`}>
                 {e.task}
               </h1>
               <div>
-                <input type="checkbox" />
+                <input type="checkbox" onClick={()=> completedTask(e.id)} />
                 <button className="p-4 border-t-emerald-600 rounded-md bottom-8 hover:text-white hover:bg-black">
                   Edit
                 </button>
-                <button className="p-4 border-t-emerald-600 rounded-md bottom-8 hover:text-white hover:bg-black">
+                <button onClick={()=>deleteTask(e.id)} className="p-4 border-t-emerald-600 rounded-md bottom-8 hover:text-white hover:bg-black">
                   Delete
                 </button>
               </div>
